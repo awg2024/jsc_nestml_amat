9e868d684ade193f2cda884e27fc6ed9f8349444
Angus Gray <55931655+awg2024@users.noreply.github.com>
2026-07-23 16:27:12 +0100

    Coincidence factor analysis (testing)

--- juypter/target/nestml_amat_module.cpp
+++ juypter/target/nestml_amat_module.cpp
@@ -19,7 +19,7 @@
 *  You should have received a copy of the GNU General Public License
 *  along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 *
-*  Generated from NESTML 8.3.0 at time: 2026-07-22 13:09:51.145303
+*  Generated from NESTML 8.3.0 at time: 2026-07-23 15:20:26.892132
 */
 
 // Include from NEST
